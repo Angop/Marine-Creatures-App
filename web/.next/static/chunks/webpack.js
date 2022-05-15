04afaf557fd18c1cd089ad52dc84296582f0da35
Akerlin <angela_rose2000@hotmail.com>
2022-05-15 13:09:16 -0700

    Added submit button to model

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5129bc7a11b2a4c0"; }
+/******/ 		__webpack_require__.h = function() { return "54248ed85a2f1837"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- web/src/pages/model.tsx
+++ web/src/pages/model.tsx
@@ -1,14 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import React from "react";
 import { UploadField } from "../components/UploadField";
+import { Wrapper } from "../components/Wrapper";
 
 interface modelProps {}
 
 export const Model: React.FC<modelProps> = ({}) => {
   return (
-    <Box mt={4}>
-      <UploadField label="uploadfile" accepts="image/*"/>
-    </Box>
+    // TODO: authenticate user and only allow them to see this page if they are logged in
+    <Wrapper>
+      <Formik
+        initialValues={{ image: null }}
+        onSubmit={async (values) => {
+          // console.log(values);
+          //   const response = await uploadField(values);
+          //   return response;
+          return null;
+        }}
+      >
+        {(values, handleChange) => (
+          <Form>
+            <Box mt={4}>
+              <UploadField label="uploadfile" accepts="image/*" />
+            </Box>
+            <Button mt={4} type="submit">
+              Submit
+            </Button>{" "}
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+
+    // TODO: add a button to upload a file
+    // TODO: display uploaded file
   );
 };
 
